f7e045e2616dd5d7360757c346d04960a5f0bdd2
Alexis <olivarezalexi749@gmail.com>
2025-06-21 20:11:43 -0300

    add copy to clipboard functionality and buttons for icons in IconsWithSearch component

--- src/components/icons-list.tsx
+++ src/components/icons-list.tsx
@@ -15,6 +15,12 @@ export default function IconsWithSearch({ icons }: Props) {
 		setSearchQuery(event.target.value)
 	}
 
+	const copyToClipboard = (text: string) => {
+		navigator.clipboard.writeText(text).then(() => {
+			alert("Copied to clipboard: " + text)
+		})
+	}
+
 	return (
 		<main>
 			<section className="relative w-full text-base">
@@ -46,7 +52,7 @@ export default function IconsWithSearch({ icons }: Props) {
 			</section>
 			<section className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 p-4">
 				{filteredIcons.map(
-					({ id_team, slug, name, slug_league, league_name }) => (
+					({ id_team, slug, name, slug_league, league_name, icon }) => (
 						<article
 							key={id_team}
 							className="flex flex-col items-center justify-center rounded-md border border-zinc-200 p-4 text-zinc-900 transition-colors duration-100 dark:border-zinc-800 dark:text-zinc-100"
@@ -65,6 +71,40 @@ export default function IconsWithSearch({ icons }: Props) {
 								className="flex cursor-pointer items-center justify-center gap-x-1 rounded-md bg-zinc-50 px-2 py-1 text-xs font-medium text-zinc-600 capitalize ring-1 ring-zinc-600/10 transition-all duration-300 ease-in-out ring-inset dark:bg-zinc-400/10 dark:text-zinc-400 dark:ring-zinc-400/30">
 								{league_name}
 							</a>
+							<div className="mt-2 flex items-center justify-center">
+								<button
+									onClick={() => copyToClipboard(icon)}
+									className="hover:bg-accent text-accent-foreground inline-flex size-7 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:ring-1 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4 [&_svg]:shrink-0">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth="2"
+										stroke="currentColor">
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+										/>
+									</svg>
+								</button>
+								<button
+									onClick={() => alert("Proximamente")}
+									className="hover:bg-accent text-accent-foreground inline-flex size-7 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:ring-1 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4 [&_svg]:shrink-0">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth="2"
+										stroke="currentColor">
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+										/>
+									</svg>
+								</button>
+							</div>
 						</article>
 					)
 				)}
